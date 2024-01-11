fe3f7916c27ca885cae8950f0186411be0b8e594
takafumi11 <noirit7@gmail.com>
2024-01-11 23:37:58 +0900

    Applied MCV architecture and added Repositoty, Controller, Entity, Service

---
+++ src/main/java/com/example/todoapi/repositoty/sample/SampleRecord.java
@@ -0,0 +1,8 @@
+package com.example.todoapi.repositoty.sample;
+
+import lombok.Value;
+
+@Value
+public class SampleRecord {
+    String content;
+}
---
+++ src/main/java/com/example/todoapi/service/sample/ServiceEntity/SampleEntity.java
@@ -0,0 +1,8 @@
+package com.example.todoapi.service.sample.ServiceEntity;
+
+import lombok.Value;
+
+@Value
+public class SampleEntity {
+    String content;
+}
